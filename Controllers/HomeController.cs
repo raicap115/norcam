f6adc0ee8672a962312e2111d4d84adb69965e4b
Luis Torres <ltorcerps4@gmail.com>
2020-12-10 23:04:02 -0500

    login clientes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace norcam.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
