0bf400c19571f7b6395574bb6a1a6208861bc3e9
Raí Campos <raicampas23@gmail.com>
2020-12-22 01:31:47 -0500

    commit

--- Controllers/OrdenesController.cs
+++ Controllers/OrdenesController.cs
@@ -6,14 +6,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using norcam.Models;
-
+using norcam.Data;
 namespace norcam.Controllers
 {
     public class OrdenesController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public OrdenesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
         public IActionResult Index()
         {
-            return View();
+            var orden = _context.Ordenes.ToList();
+            return View(orden);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Ordenes objOrden)
+        {
+            if (ModelState.IsValid) {
+
+                _context.Add(objOrden);
+                _context.SaveChanges();
+            }
+
+            return View("Index");
         }
     }
 }
--- Models/Ordenes.cs
+++ Models/Ordenes.cs
@@ -5,16 +5,27 @@ namespace norcam.Models
 {
     public class Ordenes
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
+        [Display(Name="Fecha de recepción")]
         public string fec_rec { get; set; }
+        [Display(Name="Código de cliente")]
         public Cliente id_cliente { get; set; }
+        [Display(Name="Proveedor")]
         public string proveedor { get; set; }
+        [Display(Name="Contenido")]
         public string contenido { get; set; }
+        [Display(Name="Peso")]
         public string peso { get; set; }
+        [Display(Name="Ubicación")]
         public string ubicacion { get; set; }
+        [Display(Name="Régimen")]
         public string regimen { get; set; }
+        [Display(Name="Fec. Numeración")]
         public string fec_num { get; set; }
+        [Display(Name="Fec. Entrega")]
         public string fec_entrega { get; set; }
+        [Display(Name="T/C. - S/")]
         public double valor_num { get; set; }
     }
 }
